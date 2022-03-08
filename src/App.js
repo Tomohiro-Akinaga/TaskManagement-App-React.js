4373c8f98ff2864a207198d486f008faff3362ba
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-07 17:33:20 -0800

    added firebase for hosting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-    return <input type="text"></input>;
+    return (
+        <>
+            <input type="text"></input>
+            <button type="button">
+                Sign Up
+            </button>
+        </>
+    );
 }
 
 export default App;
