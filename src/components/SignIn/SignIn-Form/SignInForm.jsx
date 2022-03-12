e257d8fc8dec2f6abe82a0f96b88a8c6fe8fc48f
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-12 15:23:29 -0800

    created signIn form

--- src/components/SignIn/SignIn-Form/SignInForm.jsx
+++ src/components/SignIn/SignIn-Form/SignInForm.jsx
@@ -3,9 +3,17 @@ import FormStyle from "./SignInForm.module.scss";
 export default function SignInForm() {
     return (
         <form type="submit" className={FormStyle.form}>
-            <input type="text" placeholder="email" className={FormStyle.email} />
-            <input type="text" className={FormStyle.password} />
-            <button type="submit" className={FormStyle.signIn}></button>
+            <input
+                type="text"
+                placeholder="email"
+                className={FormStyle.email}
+            />
+            <input
+                type="text"
+                placeholder="password"
+                className={FormStyle.password}
+            />
+            <button type="submit" className={FormStyle.signIn}>Sign In</button>
         </form>
     );
 }
