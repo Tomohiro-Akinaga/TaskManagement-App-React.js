12dd7db80e18b93634387c1847feafdc5d8b9c3f
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-16 20:23:49 -0700

    added important list template

--- src/components/Home/Main/Main-ImportantList/ImportantList.jsx
+++ src/components/Home/Main/Main-ImportantList/ImportantList.jsx
@@ -1,5 +1,28 @@
 import ImportantListStyle from "./ImportantList.module.scss";
+import isImportantImg from "../../../../resources/img/whole-star.png";
 
 export default function ImportantList() {
-    return <h1 className={ImportantListStyle.container}>Important</h1>;
+    return (
+        <div className={ImportantListStyle.container}>
+            <ul>
+                <li
+                    className={ImportantListStyle.list}
+                    // data-id={id}
+                    // data-complete={complete}
+                    // data-important={important}
+                >
+                    <button
+                        className={ImportantListStyle.buttonImportant}
+                        // onClick={handleClickImportant}
+                    >
+                        <img
+                            src={isImportantImg}
+                            className={ImportantListStyle.imageImportant}
+                        />
+                    </button>
+                    <p className={ImportantListStyle.text}></p>
+                </li>
+            </ul>
+        </div>
+    );
 }
